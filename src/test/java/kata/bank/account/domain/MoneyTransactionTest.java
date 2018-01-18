755b6b011c6bf2076c8839959cf9f5f765aabec7
m-slimani <35559396+m-slimani@users.noreply.github.com>
2018-01-18 15:47:22 +0100

    Refacto suite

--- src/test/java/kata/bank/account/domain/MoneyTransactionTest.java
+++ src/test/java/kata/bank/account/domain/MoneyTransactionTest.java
@@ -8,12 +8,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MoneyTransactionTest {
+	
+	private static final String THOUSAND = "1000.00";
+	private static final String HUNDRED = "100.00";
+	private static final String NINE_HUNDRED = "900.00";
 
 	@Test
 	public void TestPerformWithdraw() {
-		Amount currentBalance = Amount.getAmountOf(new BigDecimal("1000.00"), Currency.getInstance(Locale.FRANCE));
-		Amount withdrawHundred = Amount.getAmountOf(new BigDecimal("100.00"), Currency.getInstance(Locale.FRANCE));
-		Amount expectedBalance = Amount.getAmountOf(new BigDecimal("900.00"), Currency.getInstance(Locale.FRANCE));
+		Amount currentBalance = Amount.getAmountOf(new BigDecimal(THOUSAND), Currency.getInstance(Locale.FRANCE));
+		Amount withdrawHundred = Amount.getAmountOf(new BigDecimal(HUNDRED), Currency.getInstance(Locale.FRANCE));
+		Amount expectedBalance = Amount.getAmountOf(new BigDecimal(NINE_HUNDRED), Currency.getInstance(Locale.FRANCE));
 		
 		//Declare a transaction with a current balance
 		MoneyTransaction transaction = new MoneyTransaction(withdrawHundred);
